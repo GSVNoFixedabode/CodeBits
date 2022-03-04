f2dd2a8791b0a7760cdd70ca60614f1285f832b2
GSVNoFixedabode <gsvnofixedabode@gmail.com>
2022-03-04 20:45:24 +1300

    Update Pico_LED_Ring.py
    Updated code for button pressing

--- Pico_LED_Ring.py
+++ Pico_LED_Ring.py
@@ -1,5 +1,5 @@
 from machine import Pin
-import utime, array, time, rp2
+import utime, array, time, rp2, random
 # import machine
 ############################################
 #
@@ -17,9 +17,12 @@ button_cycle = 1
 def button1_pressed(change):
     global button_cycle
     button_cycle += 1
-    button_cycle = button_cycle % 5
+    button_cycle = button_cycle % 6
+    if button_cycle == 0:
+        button_cycle = 1
     print(button_cycle)
     alexa_zip()
+
     
 # we define button1 as being connected to GP14 and to use the internal Pico PULL_DOWN resistor
 button1 = Pin(14, Pin.IN, Pin.PULL_UP)
@@ -42,16 +45,8 @@ BLUE = '#0000ff'
 PURPLE = '#b400ff'
 WHITE = '#ffffff'
 COLORS = (BLACK, RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE)
-
 # breathe color specifications
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-yellow = (255,255,0)
-cyan = (0,255,255)
-white = (255,255,255)
-blank = (0,0,0)
-colors = [blue,yellow,cyan,red,green,white]
+colors = [BLUE,YELLOW,CYAN,RED,GREEN,WHITE]
 
 
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT,
@@ -93,18 +88,111 @@ def set_24bit(ii, color): # set colors to 24-bit format inside pixel_array
     
 def hex_to_rgb(hex_val):
     return tuple(int(hex_val.lstrip('#')[ii:ii+2],16) for ii in (0,2,4))
-
-def pixels_set(i, color):
-    pixel_array[i] = (color[1]<<16) + (color[0]<<8) + color[2] # set 24-bit color
-    
+ 
 #
 ########################################### End WS2812 definitions
 
+###### Breathing loop
 #LEDS Display routines
+def breathing_led(color):
+    global button_cycle
+    step = 5
+    breath_amps = [ii for ii in range(0,255,step)]
+    breath_amps.extend([ii for ii in range(255,-1,-step)])
+    for ii in breath_amps:
+        if button_cycle != 3:
+            break
+        for jj in range(len(pixel_array)):
+            set_24bit(jj, color) # show all colors
+        update_pix(ii/255)
+        time.sleep(0.01)
+
+def breathing():
+    for color in colors: # emulate breathing LED (similar to Amazon's Alexa)
+        breathing_led(color)
+        time.sleep(0.1) # wait between colors
+####### End Breathing loop
+        
+def alexa_zip():
+# turn off LEDs using the Alexa zipper-type turnoff
+    for ii in range(int(len(pixel_array)/2)):
+        set_24bit(ii,'#000000') # turn off positive side
+        set_24bit(int(len(pixel_array)-ii-1),'#000000') # turn off negative side 
+        update_pix() # update
+        time.sleep(0.02) # wait
+
+def ring_spin(color,blank):
+    global button_cycle
+#    blank = "#ffffff"
+    cycles = 5 # number of times to cycle 360-degrees
+    for ii in range(int(cycles*len(pixel_array))+1):
+        for jj in range(len(pixel_array)):
+            if jj==int(ii%led_count): # in case we go over number of pixels in array
+                set_24bit(jj,color) # color and loop a single pixel
+            else:
+                set_24bit(jj,blank) # turn others off
+        update_pix() # update pixel colors
+        time.sleep(0.05) # wait 50ms
+        if button_cycle != 5:
+           break
+
+####### start RING Spiner
+def rgb2hex(r,g,b):
+    return "#{:02x}{:02x}{:02x}".format(r,g,b)
+
+def ring_spinner():
+    global button_cycle
+    spot = '#ff0000' # looping color starts red
+    wipe = '#ffffff'
+    for rr in range(200):
+        if button_cycle != 5:
+           break
+        ring_spin(spot,wipe)
+        wipe = spot # color for other pixels
+        c1 = random.randint(1,255)
+        c2 = random.randint(1,255)
+        c3 = random.randint(1,255)
+# next colour
+        spot = rgb2hex(c1,c2,c3)
+####### End Ring Spinner
+        
+####### Google loop
+def google_loop():
+        # Create Google Home four color rotation scheme
+    google_colors = ['#4285f4','#ea4335','#fbbc05','#34a853'] # hex colors by Google
+    cycles = 2 # number of times to cycle 360-degrees
+    for jj in range(int(cycles*len(pixel_array))):
+        for ii in range(len(pixel_array)):
+            if ii%int(len(pixel_array)/4)==0: # 90-degree leds only
+                set_24bit((ii+jj)%led_count,google_colors[int(ii/len(pixel_array)*4)])
+            else:
+                set_24bit((ii+jj)%led_count,'#000000') # other pixels blank
+        update_pix() # update pixel colors
+        time.sleep(0.05) # wait between changes
+####### End Google Loop
+        
+####### Google loop       
+def amazon_loop():
+    # Create Amazon Alexa rotation wheel
+    amazon_colors = ['#00dbdc','#0000d4'] # hex colors by Amazon
+    light_width = 3 # width of rotating led array
+    cycles = 2 # number of times width rotates 360-deg
+    for jj in range(int(cycles*len(pixel_array))):
+        for ii in range(len(pixel_array)):
+            if ii<light_width: 
+                set_24bit((ii+jj)%led_count,amazon_colors[0])
+            else:
+                set_24bit((ii+jj)%led_count,amazon_colors[1]) # other pixels blank
+        update_pix() # update pixel colors
+        time.sleep(0.03) # wait between changes
+    time.sleep(0.1)
+####### End Amazon Loop
+    
+####### Temperature Loop
 # Code to simplify...
 def paint_leds(colourIn, intTemp):
     global led_count
-    print(colourIn)
+#    print(colourIn)
     if intTemp == 0:
         intTemp = 10
     print(intTemp)
@@ -118,91 +206,10 @@ def paint_leds(colourIn, intTemp):
 # done. Now update strip
     update_pix()
 
-def breathing_led(color):
-    step = 5
-    breath_amps = [ii for ii in range(0,255,step)]
-    breath_amps.extend([ii for ii in range(255,-1,-step)])
-    for ii in breath_amps:
-        for jj in range(len(pixel_array)):
-            pixel_set(jj, color) # show all colors
-        pixels_show(ii/255)
-        time.sleep(0.02)
-        
-def alexa_zip():
-# turn off LEDs using the Alexa zipper-type turnoff
-    for ii in range(int(len(pixel_array)/2)):
-        set_24bit(ii,'#000000') # turn off positive side
-        set_24bit(int(len(pixel_array)-ii-1),'#000000') # turn off negative side 
-        update_pix() # update
-        time.sleep(0.02) # wait
-
-def ring_spin(color,blank):
-#blank = (255,255,255)
-    cycles = 5 # number of times to cycle 360-degrees
-    for ii in range(int(cycles*len(pixel_array))+1):
-        for jj in range(len(pixel_array)):
-            if jj==int(ii%led_count): # in case we go over number of pixels in array
-                set_24bit(jj,color) # color and loop a single pixel
-            else:
-                set_24bit(jj,blank) # turn others off
-        update_pix() # update pixel colors
-        time.sleep(0.05) # wait 50ms
-
-####### start RING Spiner
-def ring_spinner():
-    spot = (255,0,0) # looping color starts red
-    wipe = (255,255,255)
-    for rr in range(2000):
-        ring_spin(spot,wipe)
-        wipe = spot # color for other pixels
-        c1 = random.randint(1,255)
-        c2 = random.randint(1,255)
-        c3 = random.randint(1,255)
-# next colour
-        spot = (c1,c2,c3)
-####### End Ring Spinner
-        
-###### Breathing loop
-def breathing():
-    for color in colors: # emulate breathing LED (similar to Amazon's Alexa)
-        breathing_led(color)
-        time.sleep(0.1) # wait between colors
-####### End Breathing loop
-        
-####### Google loops
-def google_loop():
-        # Create Google Home four color rotation scheme
-    google_colors = ['#4285f4','#ea4335','#fbbc05','#34a853'] # hex colors by Google
-    cycles = 5 # number of times to cycle 360-degrees
-    for jj in range(int(cycles*len(pixel_array))):
-        for ii in range(len(pixel_array)):
-            if ii%int(len(pixel_array)/4)==0: # 90-degree leds only
-                set_24bit((ii+jj)%led_count,google_colors[int(ii/len(pixel_array)*4)])
-            else:
-                set_24bit((ii+jj)%led_count,'#000000') # other pixels blank
-        update_pix() # update pixel colors
-        time.sleep(0.05) # wait between changes
-        
-    # Create Amazon Alexa rotation wheel
-    amazon_colors = ['#00dbdc','#0000d4'] # hex colors by Amazon
-    light_width = 3 # width of rotating led array
-    cycles = 3 # number of times width rotates 360-deg
-    for jj in range(int(cycles*len(pixel_array))):
-        for ii in range(len(pixel_array)):
-            if ii<light_width: 
-                set_24bit((ii+jj)%led_count,amazon_colors[0])
-            else:
-                set_24bit((ii+jj)%led_count,amazon_colors[1]) # other pixels blank
-        update_pix() # update pixel colors
-        time.sleep(0.03) # wait between changes
-    time.sleep(0.5)
-####### End Google Loops
-    
-####### Temperature Loop
 def temperature_display():
     reading = sensor_temp.read_u16() * conversion_factor 
     temperature = int(27 - (reading - 0.706)/0.001721)
-    print(temperature)
+#    print(temperature)
     if temperature <= 0:
         paint_leds(WHITE, abs(temperature))
     elif temperature <= 10:
@@ -228,7 +235,7 @@ state_mach = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=machine.Pi
 state_mach.active(1)
 # Range of LEDs stored in an array
 pixel_array = array.array("I", [0 for _ in range(led_count)])
-
+    
 while True:
     # onboard temperature #####################
     while button_cycle == 1:
@@ -237,12 +244,11 @@ while True:
         temperature_display()
     while button_cycle == 2:
         google_loop()
- #   while button_cycle == 3:
- #       breathing()
+    while button_cycle == 3:
+        breathing()
     while button_cycle == 4:
+        amazon_loop()
+    while button_cycle == 5:
         ring_spinner()
     button_cycle = 1
-
-# turn off LEDs using the Alexa zipper-type turnoff
-# alexa_zip()
-    
+   
